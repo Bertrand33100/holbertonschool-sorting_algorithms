b8a7f4d5a01a26c3ec8cb1d02da91b47635e8084
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-08-04 14:36:53 +0200

    1. Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,40 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Trie une liste doublement chaînée
- *                       avec l’algorithme du tri par insertion.
- * @list: Pointeur vers le pointeur de tête de la liste.
- *
- * Description :
- * Le tri par insertion déplace chaque élément vers la gauche
- * tant qu’il est plus petit que son précédent.
+ * insertion_sort_list - Sort a doubly linked list using insertion sort
+ * @list: Pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *tmp;
+	listint_t *current, *tmp;
 
-    if (!list || !*list || !(*list)->next)
-        return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
-    current = (*list)->next;
+	current = (*list)->next;
 
-    while (current)
-    {
-        tmp = current;
-        current = current->next;
+	while (current)
+	{
+		tmp = current;
+		current = current->next;
 
-        /* Déplacement de tmp vers la gauche si nécessaire */
-        while (tmp->prev && tmp->prev->n > tmp->n)
-        {
-            /* Retirer tmp de sa position actuelle */
-            tmp->prev->next = tmp->next;
-            if (tmp->next)
-                tmp->next->prev = tmp->prev;
+		while (tmp->prev && tmp->prev->n > tmp->n)
+		{
+			tmp->prev->next = tmp->next;
+			if (tmp->next)
+				tmp->next->prev = tmp->prev;
 
-            /* Insérer tmp avant son précédent */
-            tmp->next = tmp->prev;
-            tmp->prev = tmp->prev->prev;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
 
-            if (tmp->prev)
-                tmp->prev->next = tmp;
-            else
-                *list = tmp;
+			if (tmp->prev)
+				tmp->prev->next = tmp;
+			else
+				*list = tmp;
 
-            tmp->next->prev = tmp;
+			tmp->next->prev = tmp;
 
-            print_list(*list);
-        }
-    }
+			print_list(*list);
+		}
+	}
 }
