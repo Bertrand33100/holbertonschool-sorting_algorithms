cb0b4c50b5ed61ad60ab2843aba8a9c32257129c
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-08-04 20:52:09 +0200

    2. Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ *                  using the Selection sort algorithm
+ * @array: The array to sort
+ * @size: The size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, min_index;
+    int tmp;
+
+    if (!array || size < 2)
+        return;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min_index = i;
+
+        /* Trouver l'indice du plus petit élément */
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_index])
+                min_index = j;
+        }
+
+        /* Si un plus petit élément est trouvé → swap */
+        if (min_index != i)
+        {
+            tmp = array[i];
+            array[i] = array[min_index];
+            array[min_index] = tmp;
+
+            print_array(array, size);
+        }
+    }
+}
