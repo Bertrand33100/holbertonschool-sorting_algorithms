b8f2ce07f4888a7cbae08ad45d1ad6bd5e05daf4
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-08-04 21:05:04 +0200

    3. Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,75 +1,77 @@
 #include "sort.h"
 
 /**
- * lomuto_partition - Partitions an array using the Lomuto scheme
- * @array: Array to sort
- * @low: Starting index
- * @high: Ending index
- * @size: Size of the array
+ * lomuto_partition - Partition selon le schéma de Lomuto
+ * @array: Tableau à trier
+ * @low: Indice de début
+ * @high: Indice de fin (pivot)
+ * @size: Taille du tableau
  *
- * Return: Pivot index
+ * Return: Indice final du pivot
  */
 static int lomuto_partition(int *array, int low, int high, size_t size)
 {
-	int pivot, i, j, tmp;
+    int pivot = array[high];
+    int i = low;
+    int j, tmp;
 
-	pivot = array[high];
-	i = low;
+    for (j = low; j < high; j++)
+    {
+        /* Si l'élément est plus petit que le pivot */
+        if (array[j] < pivot)
+        {
+            /* Swap uniquement si les valeurs sont différentes */
+            if (i != j && array[i] != array[j])
+            {
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+                print_array(array, size);
+            }
+            i++;
+        }
+    }
 
-	for (j = low; j < high; j++)
-	{
-		if (array[j] < pivot)
-		{
-			if (i != j)
-			{
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
-				print_array(array, size);
-			}
-			i++;
-		}
-	}
+    /* Swap final du pivot : uniquement si les valeurs sont différentes */
+    if (array[i] != array[high])
+    {
+        tmp = array[i];
+        array[i] = array[high];
+        array[high] = tmp;
+        print_array(array, size);
+    }
 
-	if (i != high)
-	{
-		tmp = array[i];
-		array[i] = array[high];
-		array[high] = tmp;
-		print_array(array, size);
-	}
-
-	return (i);
+    return (i);
 }
 
 /**
- * quick_sort_rec - Sorts recursively using Quick sort
- * @array: Array to sort
- * @low: Starting index
- * @high: Ending index
- * @size: Size of the array
+ * quick_sort_rec - Fonction récursive du Quick sort
+ * @array: Tableau à trier
+ * @low: Indice de début
+ * @high: Indice de fin
+ * @size: Taille du tableau
  */
 static void quick_sort_rec(int *array, int low, int high, size_t size)
 {
-	int pivot;
+    int pivot;
 
-	if (low < high)
-	{
-		pivot = lomuto_partition(array, low, high, size);
-		quick_sort_rec(array, low, pivot - 1, size);
-		quick_sort_rec(array, pivot + 1, high, size);
-	}
+    if (low < high)
+    {
+        pivot = lomuto_partition(array, low, high, size);
+        quick_sort_rec(array, low, pivot - 1, size);
+        quick_sort_rec(array, pivot + 1, high, size);
+    }
 }
 
 /**
- * quick_sort - Sorts an array of integers in ascending order
- * @array: Array to sort
- * @size: Size of the array
+ * quick_sort - Tri rapide (Lomuto)
+ * @array: Tableau à trier
+ * @size: Taille du tableau
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	quick_sort_rec(array, 0, (int)(size - 1), size);
+    quick_sort_rec(array, 0, (int)(size - 1), size);
 }
